326500319fbc37dff2f62aa1afb7967eb27c293a
k-i-k-a <100940677+k-i-k-a@users.noreply.github.com>
2024-06-27 19:02:31 +0300

    Added Facebook sharing functionality to createPosts

--- POC/JCBackend/POC_backend/controllers/postsController.py
+++ POC/JCBackend/POC_backend/controllers/postsController.py
@@ -1,8 +1,10 @@
 from utils import db
 from django.http import JsonResponse
 import json
+import requests
 
 posts = db['posts']
+access_token = 'YOUR_ACCESS_TOKEN'
 
 def createPosts(request):
     if request.method == 'POST':
@@ -15,6 +17,14 @@ def createPosts(request):
             'likes': 0,
             'dislies': 0,
         }
+        message="by "+post_data.get('author')+": "+ post_data.get('title')+"\n" +post_data.get('content')
+        payload = {
+                        'access_token': access_token,
+                        'message': message
+                    }
         posts.insert_one(post)
+        url = 'https://graph.facebook.com/296731426868004/feed'
+        response = requests.post(url, data=payload)
+        print(response.json())
         return JsonResponse({'message': 'Post created successfully'}, status=201)
     return JsonResponse({'message': 'Invalid request'}, status=400)
